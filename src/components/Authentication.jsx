0a25d3a061add2cb19d4984ebfa01dbade726844
highlander santos <highlander@lanlink.com.br>
2022-06-30 12:03:04 -0300

    token - axios

--- src/components/Authentication.jsx
+++ src/components/Authentication.jsx
@@ -5,8 +5,6 @@ import axios from "axios";
 const token =
   "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6InhtTklxZGRDalZJZGpfdlNZUk42ViJ9.eyJpc3MiOiJodHRwczovL2Rldi1taTUxcDFsdi51cy5hdXRoMC5jb20vIiwic3ViIjoiZWlQc0VLS3JYc2JTZTk2clFHZHNmWENkV0hQVUt5S09AY2xpZW50cyIsImF1ZCI6Imh0dHBzOi8vZnVsbC1zdGFjay1leGFtLWdldC11cGxvYWQtZGV0YWlscy1hcGkuY29tIiwiaWF0IjoxNjU1OTQ1NzU3LCJleHAiOjE2NTY1NTA1NTcsImF6cCI6ImVpUHNFS0tyWHNiU2U5NnJRR2RzZlhDZFdIUFVLeUtPIiwiZ3R5IjoiY2xpZW50LWNyZWRlbnRpYWxzIn0.ArPAif9R-ZP9_Rc3Y_iM0EsxGRfdaFz4MT7IPrtnzDfYNOfR6iszF_Y6ndbDIAUL_lGwF6iu1QguVj1G6TSEipWxrM2xsd46Ejs-Q8GqKjzFkSLink68ER2K99KWgk1_T9y6Z_kYeTyDOECYIiJSuEhnuZO8RYwMMWYXdBWfqa6IfnDcnu8f6XhTWmZsvqWeLSbbiEkq4qXLDRD4pBnFbAJ8eu_1LIRHfEkK3aCyS9NHr7JtKc6UnhZZAgH8sEh066pduTl9IoI2aS6pgJhCV9ClV5v9KUBIcgs_k5tuUbuEs66YerzvZEqx7oDdPI_n0Cilq_SYGY5SyE5la2cMGw";
 
-
-
 axios.interceptors.request.use(
   async (config) => {
     config.headers.Authorization = `Bearer ${token}`;
@@ -21,14 +19,18 @@ const Sobre = () => {
   const [response, setResponse] = useState({});
 
   const fetchData = async () => {
-    try {
-      const result = await axios.get('https://8wvaarvrsc.execute-api.sa-east-1.amazonaws.com/default/fullstack_exam_get_presigned_url', {
-        headers: { 'Content-Type': 'application/json'}
-      }).then((response) => response.data)
-      setResponse(result);
-    } catch (error) {
-      console.error(error);
-    }
+    axios
+      .get("https://8wvaarvrsc.execute-api.sa-east-1.amazonaws.com/default/fullstack_exam_get_presigned_url", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   };
 
   useEffect(() => {
@@ -36,7 +38,7 @@ const Sobre = () => {
   }, []);
   console.log(response);
 
-  return <h1>Sobre</h1>;
+  return <h1>{response}</h1>;
 };
 
 export default Sobre;
